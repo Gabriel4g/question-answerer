55f14f700c7fa1157c084045364e1d27e5963d56
Gabriel4g <gabrielprogramador11@gmail.com>
2022-07-26 20:50:32 -0500

    Desestruturando o objeto de perguntas

--- main.js
+++ main.js
@@ -14,16 +14,16 @@ function askQuestion() {
     const objectQuestions = {
         emptyQuestion: '',
         devBrabo: 'Vou ser um programador brabo?',
-        question1: "Qual o melhor jogo do momento?",
-        question2: 'Quem te criou?',
-        question3: 'Nescau ou toddy?',
-        question4: 'Me fale uns jogos legais',
-        question5: 'Valorant é fps tatico?',
-        question6: 'Quem desenvolveu o valorant?',
+        bestGame: "Qual o melhor jogo do momento?",
+        creatorApp: 'Quem te criou?',
+        NescauAndToddy: 'Nescau ou toddy?',
+        gamesGood: 'Me fale uns jogos legais',
+        valorant: 'Valorant é fps tatico?',
+        creatorValorant: 'Quem desenvolveu o valorant?',
         question7: 'Quem nasceu primeiro, o ovo ou a galinha?',
-        question8: 'Quem é o melhor gamer do Brasil?',
-        question9: 'Qual foi o melhor console de todos os tempos?',
-        question10: 'Qual é o jogo que nunca vai ser superado?'
+        bestGamer: 'Quem é o melhor gamer do Brasil?',
+        consoleLog: 'Qual foi o melhor console de todos os tempos?',
+        question10: 'Qual é o jogo que nunca vai ser superado?',
     }
 
     if (input_quest.value != objectQuestions) {
@@ -41,71 +41,75 @@ function askQuestion() {
             imageGame.src = "./assets/game.png"
             return;
         }, 5000)
-        clearTimeout()
     }
+
+    // Desestruturação do objeto de perguntas
+    const {
+    emptyQuestion, devBrabo, bestGame, creatorApp, NescauAndToddy, gamesGood, valorant, 
+    creatorValorant, question7, bestGamer, consoleLog, question10} = objectQuestions;
 
     switch (input_quest.value) {
 
-        case objectQuestions.emptyQuestion:
+        case emptyQuestion:
             answer.innerHTML = "Faça uma pergunta"
             imageGame.src = "https://c.tenor.com/Se0P1hR2f4gAAAAM/suspicious-emoji.gif"
             removeHtmlAfter()
             break;
 
-        case objectQuestions.devBrabo:
+        case devBrabo:
             arrowMotivation()
             break;
 
-        case objectQuestions.question1:
+        case bestGame:
             answer.innerHTML = "Valoran... Ops, => Minecraft"
             imageGame.src = "https://c.tenor.com/mqM250ZubZwAAAAC/raze-valorant.gif"
             break;
 
-        case objectQuestions.question2:
+        case creatorApp:
             answer.innerHTML = "Gabriel, o melhorzin";
             removeHtmlAfter()
             break;
 
-        case objectQuestions.question3:
+        case NescauAndToddy:
             answer.innerHTML = "Nescau"
 
             removeHtmlAfter()
             break;
 
-        case objectQuestions.question4:
+        case gamesGood:
             answer.innerHTML = "Valorant, Apex, minecraftttkkk"
             imageGame.src = "https://i.pinimg.com/originals/34/83/b4/3483b4b5e873a4dde26a949d0b63ec99.gif"
             removeHtmlAfter()
             break;
 
-        case objectQuestions.question5:
+        case valorant:
             answer.innerHTML = "Sim, CONFIA"
             removeHtmlAfter()
             break;
 
-        case objectQuestions.question6:
+        case creatorValorant:
             answer.innerHTML = "eu"
             imageGame.src = "https://i.pinimg.com/736x/9d/4a/3b/9d4a3b8bece01eb5aef7a78eb0d7be93.jpg"
             removeHtmlAfter()
             break;
 
-        case objectQuestions.question7:
+        case question7:
             answer.innerHTML = "Galinha"
             imageGame.src = "https://gifs.eco.br/wp-content/uploads/2021/09/gifs-de-galinha-dancando-0.gif"
             removeHtmlAfter()
             break;
 
-        case objectQuestions.question8:
+        case bestGamer:
             answer.innerHTML = "Ainda pergunta? O melhor é quem me criou"
             removeHtmlAfter()
             break;
 
-        case objectQuestions.question9:
+        case consoleLog:
             answer.innerHTML = "Ps2, sem duvidas hehehehe"
             removeHtmlAfter()
             break;
 
-        case objectQuestions.question10:
+        case question10:
             answer.innerHTML = "Ben10, o melhor"
             imageGame.src = "https://studio.code.org/v3/assets/ES07HvXxl0Az8Wptu0jG_iKwwaxl__jHfmBr3wEEJuA/FakeTeemingGoldfish-max-1mb.gif"
             removeHtmlAfter()
